5fec78a39828c46f44a113c479f6f5588a6aa7ee
wlsrn3684 <50126441+wlsrn3684@users.noreply.github.com>
2021-07-13 22:57:56 +0900

    Finish add-nbo

---
+++ add-nbo.cpp
@@ -0,0 +1,44 @@
+#include <stddef.h>		// for size_t
+#include <stdint.h>		// for uint8_t
+#include <stdio.h>		// for printf
+#include <netinet/in.h> // for ntohl()
+#include <fstream>		// for open()
+
+uint32_t read_file_to_number(const char *file)
+{
+	FILE *bin;
+	bin = fopen(file, "rb");
+	if (bin == NULL)
+		return -1;
+
+	uint8_t network_buffer[4];
+	fread(network_buffer, sizeof(uint8_t), 4, bin);
+	uint32_t *p = reinterpret_cast<uint32_t *>(network_buffer);
+	uint32_t operand = ntohl(*p);
+
+	fclose(bin);
+
+	return operand;
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("invalid use... Please use the method below.\n");
+		printf("./add-nbo <first_file> <second_file>\n");
+		return -1;
+	}
+
+	uint32_t first = read_file_to_number(argv[1]);
+	uint32_t second = read_file_to_number(argv[2]);
+
+	if (first == -1 || second == -1)
+	{
+		printf("invalid input value...\n");
+		return -1;
+	}
+
+	printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", int(first), first, int(second), second, int(first + second), first + second);
+	return 0;
+}
